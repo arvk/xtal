513fc50e62255e04fca7fe63f38cdfa31c74770e
Aravind Krishnamoorthy <kris658@usc.edu>
2017-02-11 12:37:54 -0800

    Added member function to read from VASP POSCAR/CONTCAR files

--- xtal.py
+++ xtal.py
@@ -9,3 +9,37 @@ class AtTraj:
 
     def __init__(self):
         print 'Atomic trajectory initialized'
+
+    def read_snapshot_vasp(self,filename):
+        vasp_snapfile = open(filename,"r")
+
+        self.description = vasp_snapfile.readline().strip()
+        mymultiplier = float(vasp_snapfile.readline())
+
+        basisline = vasp_snapfile.readline()
+        self.box[0,0], self.box[0,1], self.box[0,2] = map(float,basisline.split())
+        basisline = vasp_snapfile.readline()
+        self.box[1,0], self.box[1,1], self.box[1,2] = map(float,basisline.split())
+        basisline = vasp_snapfile.readline()
+        self.box[2,0], self.box[2,1], self.box[2,2] = map(float,basisline.split())
+        self.box = self.box * mymultiplier
+
+        basisline = vasp_snapfile.readline()
+        atarray = basisline.split()
+
+        basisline = vasp_snapfile.readline()
+        atoms_of_type = map(int,basisline.split())
+
+        isindirectcoords = vasp_snapfile.readline()
+        isindirectcoords = isindirectcoords[0].lower() == 'd'
+
+        for index, numbers in enumerate(atoms_of_type):
+            for thistype in range (0,numbers):
+                basisline = vasp_snapfile.readline()
+                myatom = Atom()
+                myatom.afrac, myatom.bfrac, myatom.cfrac = map(float,basisline.split())
+                myatom.element = atarray[index].upper()
+                myatom.dirtocar(self.box)
+                self.atomlist.append(myatom)
+
+        vasp_snapfile.close()
