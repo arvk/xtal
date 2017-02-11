c7cf1c72b14d0d451feb00fbea80c35932492103
Aravind Krishnamoorthy <kris658@usc.edu>
2017-02-11 12:39:53 -0800

    Added member function to write VASP POSCAR/CONTCAR files

--- xtal.py
+++ xtal.py
@@ -48,3 +48,45 @@ class AtTraj:
     def sort_by_element(self):
         self.atomlist.sort(key = lambda x: x.element)
 
+
+    def write_snapshot_vasp(self,filename,write_in_direct):
+        vasp_snapfile = open(filename,"w")
+        vasp_snapfile.write(self.description+'\n')
+        vasp_snapfile.write('1.000000\n') # Default multiplier for all VASP files
+        np.savetxt(vasp_snapfile,self.box,fmt='%19.16f', delimiter = "   ", newline = "\n")
+
+        # Sort atoms by element before counting number of atoms by element
+        #self.atomlist.sort_by_element()
+        self.atomlist.sort(key = lambda x: x.element)
+        uniquesdict = {}
+        for singleatom in self.atomlist:
+            uniquesdict[singleatom.element] = singleatom.element
+        uniqueslist = uniquesdict.values()
+
+        uniquesdict = {}
+        uniquesdict1 = {}
+        for uniqueelement in uniqueslist:
+            uniquesdict[uniqueelement] = str(len([p for p in self.atomlist if p.element == uniqueelement]))
+            uniquesdict1[uniqueelement] = uniqueelement.capitalize()
+        uniqueslist = uniquesdict.values()
+        uniqueslist1 = uniquesdict1.values()
+
+        vasp_snapfile.write("  ".join(uniqueslist1)+'\n')
+        vasp_snapfile.write("  ".join(uniqueslist)+'\n')
+
+        if write_in_direct:
+            vasp_snapfile.write('Direct\n')
+            for uniqueelement in uniqueslist1:
+                subsetofatomlist = (atoms for atoms in self.atomlist if atoms.element == uniqueelement.upper())
+                for singleatom in subsetofatomlist:
+                    vasp_snapfile.write('{0}  {1}  {2}\n'.format(singleatom.afrac,singleatom.bfrac,singleatom.cfrac))
+        else:
+            vasp_snapfile.write('Cartesian\n')
+            for uniqueelement in uniqueslist1:
+                subsetofatomlist = (atoms for atoms in self.atomlist if atoms.element == uniqueelement.upper())
+                for singleatom in subsetofatomlist:
+                    vasp_snapfile.write('{0}  {1}  {2}\n'.format(singleatom.xpos,singleatom.ypos,singleatom.zpos))
+
+
+
+        vasp_snapfile.close()
